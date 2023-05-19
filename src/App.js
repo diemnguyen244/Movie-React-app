d7a6ac2a951d77110e18a3e1b7f3346d49af5fb2
Diem Nguyen <nguyentranhoaidiem@gmail.com>
2023-05-19 10:53:10 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import MovieCart from "./MovieCart";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [seachTerm, setSeachTerm] = useState("");
+  const API_URL = "http://www.omdbapi.com?apikey=fce3a303";
+  const searchMovies = async (title) => {
+    const respone = await fetch(`${API_URL}&s={title}`);
+    const data = await respone.json();
+    setMovies(data.Search);
+  };
+  const movie1 = {
+    Title: "Spider-Man Title Reveal",
+    Year: "2021",
+    imdbID: "tt14122734",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNjRjMmQ2NDQtNmI5NC00N2EwLTkwYWQtOTM2OGZjMmI5YmRjXkEyXkFqcGdeQXVyMTI0NTA1MDI3._V1_SX300.jpg",
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //fce3a303
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="search for movies"
+          value={seachTerm}
+          onChange={(e) => setSeachTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="seach"
+          onClick={() => searchMovies(seachTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCart movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>no movie</h2>
+        </div>
+      )}
     </div>
   );
 }
